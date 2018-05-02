3b385bb674147cdfdc5cc58d9c6779a005d56eaa
xhydromovie <xhydromovie@gmail.com>
2018-05-02 19:57:29 +0200

    README Updated

---
+++ src/components/AddBook.vue
@@ -0,0 +1,55 @@
+<template>
+  <form @submit.prevent="savePost">
+      <b-form-group label="Title">
+        <b-form-input type="text" v-model="model.title"></b-form-input>
+      </b-form-group>
+      <b-form-group label="Body">
+        <b-form-textarea rows="4" v-model="model.body"></b-form-textarea>
+    </b-form-group>
+  </form>
+</template>
+
+<script>
+import api from '@/api'
+export default {
+  data () {
+    return {
+      loading: false,
+      posts: [],
+      model: {}
+    }
+  },
+  async created () {
+    this.refreshPosts()
+  },
+  methods: {
+    async refreshPosts () {
+      this.loading = true
+      this.posts = await api.getPosts()
+      this.loading = false
+    },
+    async populatePostToEdit (post) {
+      this.model = Object.assign({}, post)
+    },
+    async savePost () {
+      if (this.model.id) {
+        await api.updatePost(this.model.id, this.model)
+      } else {
+        await api.createPost(this.model)
+      }
+      this.model = {} // reset form
+      await this.refreshPosts()
+    },
+    async deletePost (id) {
+      if (confirm('Are you sure you want to delete this post?')) {
+        // if we are editing a post we deleted, remove it from the form
+        if (this.model.id === id) {
+          this.model = {}
+        }
+        await api.deletePost(id)
+        await this.refreshPosts()
+      }
+    }
+  }
+}
+</script>
